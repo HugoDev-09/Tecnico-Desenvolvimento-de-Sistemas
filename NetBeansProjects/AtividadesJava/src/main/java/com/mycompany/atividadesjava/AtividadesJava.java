53e9a177998952486da2755ce709e72a2e968acf
Hugo Ruan <hugoruanmiranda@gmail.com>
2025-03-31 17:41:40 -0300

    Add files via upload

---
+++ NetBeansProjects/AtividadesJava/src/main/java/com/mycompany/atividadesjava/AtividadesJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividadesjava;
+
+/**
+ *
+ * @author Hugo53690506
+ */
+public class AtividadesJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NetBeansProjects/Teste02/src/main/java/com/mycompany/teste02/Teste02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste02;
+
+/**
+ *
+ * @author Hugo53690506
+ */
+public class Teste02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
